b7a984f9f8f025cc368d657f0e909ddbe8180c13
pedroLrock <peu.luca2005@gmail.com>
2024-09-09 11:29:31 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,64 @@
+const form = document.getElementById('form-atividade');
+const imgAprovado = '<img src="./imagens/aprovado.png" alt ="Emoji celebrando"/>';
+const imgReprovado = '<img src="./imagens/reprovado.png" alt ="Emoji decpcionado"/>';
+const atividades = [];
+const notas  = [];
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
+const notaMinima = parseFloat(prompt("Digite a nota mínima:"));
+
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionarLinha()
+    atualizarTabela()
+    atualiaMediaFinal();
+});
+
+function adicionarLinha(){
+    const inputNomeAtividade = document.getElementById('nome-atividade');
+    const inputNotaAtividade = document.getElementById('nota-atividade');
+
+    if(atividades.includes(inputNomeAtividade.value)){
+        alert(`A atividades: ${inputNomeAtividade.value} já foi inserida`);
+    } else{ 
+        atividades.push(inputNomeAtividade.value);
+        notas.push(parseFloat(inputNotaAtividade.value));
+    
+        let linha = '<tr>';
+        linha  += `<td>${inputNomeAtividade.value}</td>`;
+        linha += `<td>${inputNotaAtividade.value}</td>`;
+        linha  += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;
+        linha += '</tr>';
+    
+        linhas += linha;
+    }
+
+    inputNomeAtividade.value = '';
+    inputNotaAtividade.value = '';
+}
+
+function atualizarTabela(){
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
+
+function atualiaMediaFinal(){
+    const mediaFinal = calculaMediaFInal();
+    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
+}
+
+function calculaMediaFInal(){
+
+    let somoDasNotas = 0;
+
+        for(let i = 0; i < notas.length; i++){
+            somoDasNotas += notas[i];
+        }
+
+        return somoDasNotas / notas.length;
+
+}
